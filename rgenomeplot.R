08d7bd9553b93b1e6357273abd30ef75e3a16fb9
danielaAlvV <danialva3@icloud.com>
2024-05-02 16:14:51 +0200

    plot

--- rgenomeplot.R
+++ rgenomeplot.R
@@ -7,13 +7,13 @@ daten <- read.table("/Users/ra67tek/HPC-SS24/hpc/resultsgenome.txt", sep=";")
 head(daten)
 #daten$threads_nseq <- interaction(daten$V2, daten$V3)
 
-ggplot(daten, aes(x=V2, y=V3,col=factor(V4)))+
+ggplot(daten, aes(x=V2, y=V3,col=V4))+
   scale_y_log10()+
   scale_x_log10()+
   geom_smooth(se=F)+
   theme_bw()
 
-ggplot(daten, aes(x=V2, y=V3, group= V4, col=factor(V4)))+
+ggplot(daten, aes(x=V2, y=V3, group= V4, col=V4))+
   scale_y_log10()+
   scale_x_log10()+
   geom_smooth(se=F)+
